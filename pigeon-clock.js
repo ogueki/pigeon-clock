1560751dc95f974e130a4b1b992644b6f2cb5f54
ogueki <shinya19970712@gmail.com>
2023-09-29 15:10:41 +0900

    時刻の修正

--- pigeon-clock.js
+++ pigeon-clock.js
@@ -3,7 +3,7 @@ function updateClock() {
     const hours = now.getHours();
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
-    const timeString = `${hours}:${minutes}:${seconds}`;
+    const timeString = hours + ":" + minutes + ":" + seconds ;
     document.getElementById('digital-clock').textContent = timeString;
 
     if (minutes === 0 && seconds === 0) {
@@ -23,12 +23,12 @@ function playPigeonCry() {
 
 function changePigeonColor() {
     const pigeonWindow = document.getElementById('pigeon-character');
-    pigeonCharacter.classList.remove("pigeon-character");
-    pigeonCharacter.classList.add("pigeon-character-appear");
+    pigeonWindow.classList.remove("pigeon-character");
+    pigeonWindow.classList.add("pigeon-character-appear");
 
     setTimeout(() => {
-        pigeonCharacter.classList.remove("pigeon-character-appear");
-        pigeonCharacter.classList.add("pigeon-character");
+        pigeonWindow.classList.remove("pigeon-character-appear");
+        pigeonWindow.classList.add("pigeon-character");
     }, 10000); 
 }
 
